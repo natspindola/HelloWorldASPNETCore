f16e13c60c186674c1e128848bac9bf2e946dcc4
natspindola <natspindola@hotmail.com>
2022-03-23 19:17:34 -0300

    configuração

---
+++ Controllers/TesteController.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HelloWorldASPNETCore.Controllers
+{
+    public class TesteController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
